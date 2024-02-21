f7ff3ac2162c7dfc3272b5a649caf506b577e902
Oliver Cook <olliewatford@gmail.com>
2024-02-21 18:04:16 +0000

    final touches

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,10 @@ import Form from './components/Form'
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex flex-col items-center">
+    <main className="min-h-screen flex flex-col items-center relative">
       <h1 className="text-5xl font-bold text-center mt-8">Decimal to Binary Converter</h1>
       <Form />
+      <p className="absolute bottom-2 left-1/2 -translate-x-1/2" >Built by <a href="https://www.olliecookie.com" className="underline" target="_blank">Ollie Cook</a>&#x1f36a;</p>
     </main>
   );
 }
